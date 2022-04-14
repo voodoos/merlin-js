75979928a6a1ac258b7d1271bb988531604cc9f3
voodoos <voodoos@users.noreply.github.com>
2022-04-14 20:30:43 +0000

    deploy: 1bdd54c0389ffa04706f8467f95b56b2709515b9

---
+++ client/merlin_client.ml
@@ -0,0 +1,66 @@
+open Brr
+module Worker = Brr_webworkers.Worker
+
+(* When a query is sent to the Worker we keep the Future result in an indexed
+table so that the on_message function will be able to determine the Future when
+the answer is posted by the Worker.
+The Worker works synchronously so we expect answer to arrive in order. *)
+type worker = {
+  worker: Worker.t;
+  queue: (Protocol.answer -> unit) Queue.t
+}
+
+let add_fut worker res = Queue.add res worker.queue
+let res_fut worker v = (Queue.take worker.queue) v
+
+let make_worker url =
+  let worker = Worker.create @@ Jstr.of_string url in
+  let queue = Queue.create () in
+  let worker = { worker; queue } in
+  let on_message m =
+    let m = Ev.as_type m in
+    let data_marshaled : bytes = Brr_io.Message.Ev.data m in
+    let data : Protocol.answer = Marshal.from_bytes data_marshaled 0 in
+    res_fut worker data
+  in
+  Ev.listen Brr_io.Message.Ev.message on_message @@
+    Worker.as_target worker.worker;
+  worker
+
+(* todo share that with worker *)
+type action = Completion | Type_enclosing | Errors
+
+type errors = Protocol.error list
+
+let query ~action worker (*todo: other queries*) =
+  let fut, set  = Fut.create () in
+  add_fut worker set;
+  Worker.post worker.worker (Marshal.to_bytes action []);
+  fut
+
+let query_errors worker (source : string) =
+  let open Fut.Syntax in
+  let action = Protocol.All_errors source in
+  let+ data : Protocol.answer = query ~action worker in
+  Console.(log ["Received errors:";  data]);
+  match data with
+  | Protocol.Errors errors -> errors
+  | _ -> assert false
+
+let query_completions worker (source : string) position =
+  let open Fut.Syntax in
+  let action = Protocol.Complete_prefix (source, position) in
+  let+ data : Protocol.answer = query ~action worker in
+  Console.(log ["Received completions:";  data]);
+  match data with
+  | Protocol.Completions compl -> compl
+  | _ -> assert false
+
+let query_type worker (source : string) position =
+  let open Fut.Syntax in
+  let action = Protocol.Type_enclosing (source, position) in
+  let+ data : Protocol.answer = query ~action worker in
+  Console.(log ["Received typed enclosings:";  data]);
+  match data with
+  | Protocol.Typed_enclosings l -> l
+  | _ -> assert false
---
+++ protocol/protocol.ml
@@ -0,0 +1,42 @@
+open Merlin_kernel
+module Location = Ocaml_parsing.Location
+
+type source = string
+
+type action =
+  | Complete_prefix of source * Msource.position
+  | Type_enclosing of source * Msource.position
+  | All_errors of source
+
+type error = {
+  kind : Location.report_kind;
+  loc: Location.t;
+  main : string;
+  sub : string list;
+  source : Location.error_source;
+}
+
+type completions = {
+  from: int;
+  to_: int;
+  entries : Query_protocol.Compl.entry list
+}
+
+type is_tail_position =
+  [`No | `Tail_position | `Tail_call]
+
+(* type errors = { from: int; to_: int; entries: error list } *)
+type answer =
+ | Errors of error list
+ | Completions of completions
+ | Typed_enclosings of
+    (Location.t * [ `Index of int | `String of string ] * is_tail_position) list
+
+let report_source_to_string = function
+  | Location.Lexer   -> "lexer"
+  | Location.Parser  -> "parser"
+  | Location.Typer   -> "typer"
+  | Location.Warning -> "warning" (* todo incorrect ?*)
+  | Location.Unknown -> "unknown"
+  | Location.Env     -> "env"
+  | Location.Config  -> "config"
